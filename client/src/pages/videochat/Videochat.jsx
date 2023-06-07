9a30d43ff5fb27abd98ab46b36674e91aa48819f
AdulIT <spilberg.max@bk.ru>
2023-06-07 12:08:13 +0600

    feat: scan markup is ready, function close and request scans is ready

--- client/src/pages/videochat/Videochat.jsx
+++ client/src/pages/videochat/Videochat.jsx
@@ -11,8 +11,22 @@ import smallVideoImg from '../../assets/images/videochat/smallVideoImg.png'
 
 import cl from './Videochat.module.css'
 import Button from '../../components/UI/button/Button'
+import { useState, useRef } from 'react'
 
 const Videochat = () => {
+    const [isClicked, setIsClicked] = useState(false)
+    const btnRef = useRef(null)
+
+    function requestScans()
+    {
+        setIsClicked(true)
+    }
+
+    function closeScans()
+    {
+        setIsClicked(false)
+    }
+
     return (
         <div className={cl.videochat}>
             <header className={cl.header}>
@@ -45,15 +59,39 @@ const Videochat = () => {
                 </section>
 
                 <aside className={cl.aside}>
-                    <Button className={cl.btnReq}>Запросить сканы</Button>
-                    <p className={cl.text}>Примечание</p>
+                    <Button className={!isClicked ? cl.btnReq : cl.btnReq + ' ' + cl.btnReqHidden} onClick={requestScans}>Запросить сканы</Button>
+                    <p className={!isClicked ? cl.text : cl.text + ' ' + cl.textFirst}>Примечание</p>
                     <div className={cl.notes}>
-                        <textarea className={cl.note} placeholder='мне удобно на казахском, сухая кожа, акне'></textarea>
+                        <textarea className={!isClicked ? cl.note : cl.note + ' ' + cl.noteBig} placeholder='мне удобно на казахском, сухая кожа, акне'></textarea>
                         <Button className={cl.notesBtn}>Сохранить и отправить</Button>
                     </div>
                     <Button className={cl.btnEnd}>Прием завершен</Button>
                 </aside>
             </main>
+
+            <section className={!isClicked ? cl.scans : cl.scans + ' ' + cl.scansActive}>
+                <img className={cl.scanImg} src={scanResultSmallImg} alt='scan-img' />
+                <div className={cl.scanResultsWrapper}>
+                    <div className={cl.scanResults}>
+                        <div className={cl.scanResultItem}>
+                            <h2 className={cl.scanResultTitle}>Скан</h2>
+                            <p className={cl.scanResultInfo}>Май 30, 2023</p>
+                        </div>
+                        <div className={cl.scanResultItem}>
+                            <h2 className={cl.scanResultTitle}>Результат</h2>
+                            <p className={cl.scanResultInfoGreen}>98%</p>
+                        </div>
+                        <div className={cl.scanResultItem}>
+                            <h2 className={cl.scanResultTitle}>Заболевание</h2>
+                            <p className={cl.scanResultInfo}>Акне папуло-пустулезное</p>
+                        </div>
+                    </div>
+                    <div className={cl.scanResultBtns}>
+                        <Button className={cl.scanMagnifyBtn}>Увеличить фото</Button>
+                        <Button className={cl.scanCloseBtn} onClick={closeScans}>Закрыть</Button>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
